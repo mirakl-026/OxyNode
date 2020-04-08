429e25b3b3514280ce7407440b95185e78a98aa8
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-08 17:41:44 +0300

    Добавил дефолтные стили по-умолчанию от Никиты

---
+++ OxyNode/wwwroot/lib/OxyNode/js/default_scripts.js
@@ -0,0 +1,23 @@
+﻿let tags = document.querySelectorAll(".tag");
+let search = document.querySelector(".search");
+let popup = document.querySelector(".popup");
+
+function popup_display() {
+    popup.classList.add("display");
+}
+
+function popup_none() {
+    popup.classList.remove("display");
+}
+
+tags.forEach(function (item, i, arr) {
+    item.addEventListener('click', tag_input);
+    console.log(item);
+});
+
+function tag_input() {
+    search.value += "#" + this.innerHTML + " ";
+    // this.classList.add("tag_disable");
+    // this.classList.remove("tag");
+    // this.removeEventListener('click', tag_input);
+}
