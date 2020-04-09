490b6a4eab491bb8e88faafd51de6c0176370889
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-09 15:43:20 +0300

    Добавил базовую структуру для страницы "Контакты"

---
+++ OxyNode/Controllers/ContactsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OxyNode.Controllers
+{
+    // Страница "Контакты"
+    public class ContactsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            // await на связь с БДыыыы
+            return View();
+        }
+    }
+}
---
+++ OxyNode/Models/Contacts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OxyNode.Models
+{
+    // Модель данных для страницы "Контакты"
+    public class Contacts
+    {
+    }
+}
