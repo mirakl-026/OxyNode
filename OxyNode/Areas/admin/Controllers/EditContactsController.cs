f35e5972d2caaa525e46b51604eb16fa43dbdda7
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-09 17:59:55 +0300

    Отредактировал контроллер админка-"Конткаты" на CRUD операции

--- OxyNode/Areas/admin/Controllers/EditContactsController.cs
+++ OxyNode/Areas/admin/Controllers/EditContactsController.cs
@@ -12,15 +12,67 @@ namespace OxyNode.Areas.admin.Controllers
     [Area("admin")]
     public class EditContactsController : Controller
     {
+        private ContactsService _db;
+        public EditContactsController(ContactsService context)
+        {
+            _db = context;
+        }
+
+
         #region Edit "Contacts"
 
         // Create
+        [HttpGet]
+        public IActionResult CreateContacts()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContacts(Contacts c)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.CreateContacts(c);
+                return RedirectToAction("Index", "Panel");
+            }
+            return View(c);
+        }
 
         // Read
+        public async Task<IActionResult> ReadContacts()
+        {
+            var c = await _db.ReadContacts();
+            return View(c);
+        }
 
         // Update
+        [HttpGet]
+        public async Task<IActionResult> UpdateContacts()
+        {
+            var c = await _db.ReadContacts();
+            return View(c);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateContacts(Contacts c)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.UpdateContacts(c);
+                return RedirectToAction("Index", "Panel");
+            }
+            return View(c);
+        }
+
 
         // Delete
+        [HttpGet]
+        public async Task<IActionResult> DeleteContacts()
+        {
+            await _db.DeleteContacts();
+            return RedirectToAction("Index", "Panel");
+        }
 
         #endregion
     }
