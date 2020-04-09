2916f0a2d1afc6396f21352253a35b01416b5635
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-09 16:30:54 +0300

    Обновил модель Контактов, добавил CRUD сервис

--- OxyNode/Models/Contacts.cs
+++ OxyNode/Models/Contacts.cs
@@ -3,11 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
 namespace OxyNode.Models
 {
     // Модель данных для страницы "Контакты"
     public class Contacts
     {
+        [BsonRepresentation(BsonType.ObjectId)]
+        [HiddenInput(DisplayValue = false)]
+        public string Id { get; set; }
 
+        [Display(Name = "Название компании")]
+        [Required(ErrorMessage = "Название обязательно", AllowEmptyStrings = false)]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Название должно иметь от 4 до 50 символов")]
+        public string CompanyName { get; set; }
+
+
+        [Display(Name = "Телефон")]
+        [Required(ErrorMessage = "Телефон обязателен", AllowEmptyStrings = false)]
+        [StringLength(30, MinimumLength = 11, ErrorMessage = "Категория должна иметь от 11 до 30 символов")]
+        public List<string> PhoneNumbers { get; set; }
+
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "E-mail обязателен", AllowEmptyStrings = false)]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Производитель должен иметь от 5 до 30 символов")]
+        public string Email { get; set; }
+
+
+        // необязательная информация
+        [Display(Name = "Схема проезда")]
+        public string PathToScheme { get; set; }
+
+        [Display(Name = "Дополнительная информация - key")]
+        public string Description { get; set; }
+
+        [Display(Name = "Дополнительная информация - values")]
+        public List<string> AdditionalInfoKeys { get; set; }
+        public List<string> AdditionalInfoValues { get; set; }
     }
 }
---
+++ OxyNode/Services/ContactsService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using OxyNode.Models;
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+
+namespace OxyNode.Services
+{
+    // Сервис -> mongoDB - Contacts
+    public class ContactsService
+    {
+        private IMongoCollection<Contacts> ContactsCollection;
+        private bool HasContacts = false;   // поскольку "Контакты" в единственном числе
+
+        public ContactsService()
+        {
+            // строка подключения к БД
+            string connectionString = "mongodb://localhost:27017/OxyNode";
+            var connection = new MongoUrlBuilder(connectionString);
+
+            // получаем клиента для взаимодействия с БД
+            MongoClient client = new MongoClient(connectionString);
+
+            // получаем доступ к самой БД
+            IMongoDatabase db = client.GetDatabase(connection.DatabaseName);
+
+            // обращаемся к коллекции Contacts
+            ContactsCollection = db.GetCollection<Contacts>("ContactsCollection");
+        }
+
+        #region CRUD
+
+        // Create
+        public async Task CreateContacts(Contacts c)
+        {
+            // если до текущего момента в БД нет документа "Контакты", то документ - создаётся
+            if (HasContacts == false)
+            {
+                await ContactsCollection.InsertOneAsync(c);
+                HasContacts = true;
+            }
+            // если на текущий момент в БД уже есть документ "Контакты", то он заменяется
+            else
+            {
+                await ContactsCollection.ReplaceOneAsync(new BsonDocument(), c);
+            }            
+        }
+
+        // Read
+        public async Task<Contacts> ReadContacts()
+        {
+            // поскольку документ 1, ожно получить пустым Find
+            return await ContactsCollection.Find(new BsonDocument()).FirstOrDefaultAsync();
+        }
+
+        // Update
+        public async Task UpdateContacts(Contacts c)
+        {
+            // если до текущего момента в БД нет документа "Контакты", то документ - создаётся
+            if (HasContacts == false)
+            {
+                await ContactsCollection.InsertOneAsync(c);
+                HasContacts = true;
+            }
+            // если на текущий момент в БД уже есть документ "Контакты", то он заменяется
+            else
+            {
+                await ContactsCollection.ReplaceOneAsync(new BsonDocument(), c);
+            }
+        }
+
+        // Delete
+        public async Task DeleteContacts()
+        {
+            HasContacts = false;    // установка флага, что "Контактов" нет
+            await ContactsCollection.DeleteOneAsync(new BsonDocument());
+        }
+
+        #endregion
+    }
+}
