1beace37130e25c1930605d58f7d8eaeee941b80
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-17 17:48:10 +0300

    Добавил в контроллер админки "О нас" действия для изменения содержания, добавил представления

--- OxyNode/Areas/admin/Controllers/EditAboutController.cs
+++ OxyNode/Areas/admin/Controllers/EditAboutController.cs
@@ -4,10 +4,83 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using OxyNode.Services;
+using OxyNode.Models;
+using OxyNode.ViewModels;
+
 namespace OxyNode.Areas.admin.Controllers
 {
+    [Area("admin")]
     public class EditAboutController : Controller
     {
+        // контроллер для редактирования содржимого страницы "О нас"
+        private AboutService _db_about;
+        private AboutSertificateService _db_aboutSertificate;
+
+        public EditAboutController(AboutService aboutContext, AboutSertificateService aboutSertificateContext)
+        {
+            _db_about = aboutContext;
+            _db_aboutSertificate = aboutSertificateContext;
+        }
+
+        #region Edit "About"
+        // CREATE/ создать страницу "О нас"
+        [HttpGet]
+        public IActionResult CreateAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAbout(About avm)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db_about.CreateAbout(avm);                
+                return RedirectToAction("Index", "Panel");
+            }
+            return View(avm);
+        }
+
+        // READ/ показать страницу "О нас"
+        public async Task<IActionResult> ReadAbout()
+        {
+            AboutViewModel avm = new AboutViewModel();
+
+            avm.VM_About = await _db_about.ReadAbout();
+            avm.VM_AboutSertificates = await _db_aboutSertificate.GetAllAboutSertificates();
+
+            return View(avm);
+        }
+
+        // UPDATE/ редактировать страницу "О нас"
+        [HttpGet]
+        public async Task<IActionResult> UpdateAbout()
+        {
+            var a = await _db_about.ReadAbout();
+            return View(a);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(About a)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db_about.UpdateAbout(a);
+                return RedirectToAction("Index", "Panel");
+            }
+            return View(a);
+        }
+
+
+        // DELETE/ удалить содержимое страницы "О нас"
+        public async Task<IActionResult> DeleteAbout()
+        {
+            await _db_about.DeleteAbout();
+            return RedirectToAction("Index", "Panel");
+        }
+
+        #endregion
 
     }
 }
--- OxyNode/Models/About.cs
+++ OxyNode/Models/About.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace OxyNode.Models
 {
     // модель страницы "О нас"
     public class About
     {
+        [BsonRepresentation(BsonType.ObjectId)]
+        [HiddenInput(DisplayValue = false)]
+        public string Id { get; set; }
+
         // обязательные описание - html5
         public string DescriptionHtml5 { get; set; }
     }
