8723b50b8de06058f58fbdb4ca2b864dbf458076
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-17 16:48:11 +0300

    Добавил файл сервиса управления сертификатами

---
+++ OxyNode/Services/AboutSertificateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OxyNode.Services
+{
+    // MongoDb -> AboutSertificate / "Сертификаты" в странице "О нас"
+    public class AboutSertificateService
+    {
+    }
+}
--- OxyNode/Services/AboutService.cs
+++ OxyNode/Services/AboutService.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using OxyNode.Models;
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+
+
 namespace OxyNode.Services
 {
+    // Сервис MongoDB -> About / "О нас"
     public class AboutService
     {
+        private IMongoCollection<About> AboutCollection;
+
+
+
+
     }
 }
