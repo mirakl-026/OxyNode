90d09d712727ae41b1125896d89c65cfa31b57cd
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-17 16:53:02 +0300

    + CRUD для сервиса управления сертификатами страницы "О нас"

--- OxyNode/Services/AboutSertificateService.cs
+++ OxyNode/Services/AboutSertificateService.cs
@@ -3,10 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using OxyNode.Models;
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+
+
 namespace OxyNode.Services
 {
     // MongoDb -> AboutSertificate / "Сертификаты" в странице "О нас"
     public class AboutSertificateService
     {
+        private IMongoCollection<AboutSertificate> AboutSertificateCollection;
+
+        public AboutSertificateService()
+        {
+            // строка подключения к БД
+            string connectionString = "mongodb://localhost:27017/OxyNode";
+            var connection = new MongoUrlBuilder(connectionString);
+
+            // получаем клиента для взаимодействия с БД
+            MongoClient client = new MongoClient(connectionString);
+
+            // получаем доступ к самой БД
+            IMongoDatabase db = client.GetDatabase(connection.DatabaseName);
+
+            // обращаемся к коллекции Contacts
+            AboutSertificateCollection = db.GetCollection<AboutSertificate>("AboutSertificateCollection");
+        }
+
+        // получить все объекты коллекции - сертификаты
+        public List<AboutSertificate> GetAllAboutSertificates()
+        {
+            return AboutSertificateCollection.Find(new BsonDocument()).ToList();
+        }
+
+
+        #region CRUD
+
+        // Create
+        public async Task CreateAboutSertificate(AboutSertificate sertificate)
+        {
+            await AboutSertificateCollection.InsertOneAsync(sertificate);
+        }
+
+        // Read
+        public async Task<AboutSertificate> ReadFileAboutSertificate(string id)
+        {
+            // поскольку документ 1, ожно получить пустым Find
+            return await AboutSertificateCollection.Find(new BsonDocument("_id", new ObjectId(id))).FirstOrDefaultAsync();
+        }
+
+        // Update
+        public async Task UpdateFile(AboutSertificate sertificate)
+        {
+            await AboutSertificateCollection.ReplaceOneAsync(new BsonDocument("_id", new ObjectId(sertificate.Id)), sertificate);
+        }
+
+        // Delete
+        public async Task DeleteFile(string id)
+        {
+            await AboutSertificateCollection.DeleteOneAsync(new BsonDocument("_id", new ObjectId(id)));
+        }
+
+        #endregion
     }
 }
