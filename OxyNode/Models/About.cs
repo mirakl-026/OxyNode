6998085ca8818e9b460742a1048982dcf2220037
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-17 16:44:49 +0300

    Добавил модели - Страница "О нас" и "Сертификат компании", добавил файлы сервиса, и контроллер с представлением для админки

---
+++ OxyNode/Models/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OxyNode.Models
+{
+    // модель страницы "О нас"
+    public class About
+    {
+        // обязательные поля
+        public string Description { get; set; }
+
+        // необязательные поля
+        public List<AboutSertificate> Sertifcates { get; set; }
+    }
+}
---
+++ OxyNode/Models/AboutSertificate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace OxyNode.Models
+{
+    // модель дл сертификатов страницы "О нас"
+    public class AboutSertificate
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        [HiddenInput(DisplayValue = false)]
+        public string Id { get; set; }
+
+        [Display(Name = "Название файла сертификата")]
+        public string SertificateName { get; set; }
+
+
+        [Display(Name = "Путь к файлу сертификата")]
+        public string SertificatePath { get; set; }
+    }
+}
