77b4697a0df2bd9fdc4f4a221db7e47a9c9a7c93
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-20 17:18:02 +0300

    Начало ветки База Знаний - добавил MVC страницы "База знаний"

---
+++ OxyNode/Models/KnowledgeBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace OxyNode.Models
+{
+    // модель страницы - база знаний
+    public class KnowledgeBase
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        [HiddenInput(DisplayValue = false)]
+        public string Id { get; set; }
+
+        // обязательные описание
+        [Required(ErrorMessage = "Описание обязательно", AllowEmptyStrings = false)]
+        public string DescriptionHtml5 { get; set; }
+    }
+}
