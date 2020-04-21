29c70ad83c6d7615683f5fbb84f3ec850b9abbc1
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-21 18:54:12 +0300

    Добавил модели для БЗ

---
+++ OxyNode/Models/KB_industrySolution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OxyNode.Models
+{
+    // Модель - База знаний - отраслевое решение
+    public class KB_industrySolution
+    {
+    }
+}
---
+++ OxyNode/Models/KB_regularDocument.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OxyNode.Models
+{
+    // Модель - База знаний - нормативный документ
+    public class KB_regularDocument
+    {
+    }
+}
