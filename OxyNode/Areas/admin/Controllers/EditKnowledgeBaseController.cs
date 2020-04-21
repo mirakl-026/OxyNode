266e15206b725abbec73043c32b7de2ecf7c6fc0
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-21 16:21:10 +0300

    добавил контроллер для админки

---
+++ OxyNode/Areas/admin/Controllers/EditKnowledgeBaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OxyNode.Areas.admin.Controllers
+{
+    public class EditKnowledgeBaseController : Controller
+    {
+
+        #region Edit KnowledgeBase - main
+
+        #endregion
+
+    }
+}
