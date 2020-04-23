4be8035d830bc1b060584b4e5a3b966206126aec
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-23 15:22:03 +0300

    добавил классы сервисов и файлы иконок в resources->partial

---
+++ OxyNode/Services/KB_regularDocumentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using OxyNode.Models;
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+
+
+namespace OxyNode.Services
+{
+    public class KB_regularDocumentService
+    {
+    }
+}
