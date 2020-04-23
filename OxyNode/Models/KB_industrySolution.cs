1c0b7e7b4d3456f69d8f9839e0deff3d3e8e864f
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-23 15:09:47 +0300

    отредактировал модели нормативного документа (rd) и отраслевого решения (is)

--- OxyNode/Models/KB_industrySolution.cs
+++ OxyNode/Models/KB_industrySolution.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
 namespace OxyNode.Models
 {
     // Модель - База знаний - отраслевое решение
+    // файл на сервере - есть id, путь к файлу, краткое описание, название ссылки
     public class KB_industrySolution
     {
+        // id для хранения в БД
+        [BsonRepresentation(BsonType.ObjectId)]
+        [HiddenInput(DisplayValue = false)]
+        public string Id { get; set; }
+
+        // краткое описание
+        [Display(Name = "Краткое описание отраслевого решения")]
+        [Required(ErrorMessage = "Краткое описание обязательно", AllowEmptyStrings = false)]
+        public string is_ShortDescription { get; set; }
+
+        // текст ссылки на файл
+        [Display(Name = "Текст ссылки на файл")]
+        public string is_Name { get; set; }
+
+
+        // путь к файлу
+        [Display(Name = "Путь к файлу")]
+        public string is_Path { get; set; }
     }
 }
--- OxyNode/Models/KB_regularDocument.cs
+++ OxyNode/Models/KB_regularDocument.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
 namespace OxyNode.Models
 {
     // Модель - База знаний - нормативный документ
     public class KB_regularDocument
     {
+        // id для хранения в БД
+        [BsonRepresentation(BsonType.ObjectId)]
+        [HiddenInput(DisplayValue = false)]
+        public string Id { get; set; }
+
+        // краткое описание
+        [Display(Name = "Краткое описание нормативного документа")]
+        [Required(ErrorMessage = "Краткое описание обязательно", AllowEmptyStrings = false)]
+        public string rd_ShortDescription { get; set; }
+
+        // текст ссылки на файл
+        [Display(Name = "Текст ссылки на файл")]
+        public string rd_Name { get; set; }
+
+
+        // путь к файлу
+        [Display(Name = "Путь к файлу")]
+        public string rd_Path { get; set; }
     }
 }
