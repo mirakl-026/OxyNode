d73316ab8946a6cf4b2590bb7e5f72679c06c0f9
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-04-27 16:10:37 +0300

    редактировал контролеер админки для IS

--- OxyNode/Areas/admin/Controllers/EditIndustrySolutionsController.cs
+++ OxyNode/Areas/admin/Controllers/EditIndustrySolutionsController.cs
@@ -4,13 +4,196 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using OxyNode.Services;
+using OxyNode.ViewModels;
+using OxyNode.Models;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using MongoDB.Bson;
+
+
 namespace OxyNode.Areas.admin.Controllers
 {
+    [Area("admin")]
     public class EditIndustrySolutionsController : Controller
     {
-        public IActionResult Index()
+        // Контроллер для редактирования "Нормативные документы" - файлов этой категории
+        private string FilesPath = "/resources/knowledgeBase/industrySolutions/";
+        private IWebHostEnvironment _appEnvironment;
+        private KB_industrySolutionService _db;
+
+
+        public EditIndustrySolutionsController (IWebHostEnvironment appEnvironment, KB_industrySolutionService context)
+        {
+            _appEnvironment = appEnvironment;
+            _db = context;
+        }
+
+
+
+        #region Edit industry solutions
+
+        // загрузить файл сертификата на сервер
+        [HttpGet]
+        public IActionResult CreateIndustrySolution()
         {
             return View();
         }
+
+        [HttpPost]
+        [RequestSizeLimit(52428800)]
+        public async Task<IActionResult> CreateIndustrySolution(KB_industrySolution IS, IFormFile uploadedIS)
+        {
+            if (ModelState.IsValid & uploadedIS != null)
+            {
+                // Определение пути 
+                string path = FilesPath + uploadedIS.FileName;
+
+                // Сохранение файла на сервере
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedIS.CopyToAsync(fileStream);
+                }
+
+                // Сохранение в БД
+                // Надпись
+                if (IS.is_Name == null || IS.is_Name.Length == 0)
+                {
+                    IS.is_Name = uploadedIS.FileName;
+                }
+
+                // иконка
+                var ext = uploadedIS.FileName.Split('.').Last();
+                string icon_path = "/resources/partial/text.png";
+                if (ext.Equals("pdf"))
+                    icon_path = "/resources/partial/pdf.png";
+                else if (ext.Equals("doc"))
+                    icon_path = "/resources/partial/word.png";
+                else if (ext.Equals("zip"))
+                    icon_path = "/resources/partial/zip.png";
+                else if (ext.Equals("xls"))
+                    icon_path = "/resources/partial/excel.png";
+
+                IS.is_IconPath = icon_path;
+
+                // путь к файлу
+                IS.is_Path = path;
+
+                await _db.CreateIndustrySolution(IS);
+                return RedirectToAction("Index", "Panel");
+            }
+            return View(IS);
+        }
+
+
+        // показать файлы нормативных документов на сервере
+        [HttpGet]
+        public async Task<IActionResult> ReadAllIndustrySolutions()
+        {
+            IndustrySolutionsViewModel isvm = new IndustrySolutionsViewModel();
+            isvm.industrySolutions = await _db.GetAllIndustrySolutions();
+            return View(isvm);
+        }
+
+
+        // показать файл нормативного документа на сервере 
+        [HttpGet]
+        public async Task<IActionResult> ReadIndustrySolution(string id)
+        {
+            var IS = await _db.ReadIndustrySolution(id);
+            return View(IS);
+        }
+
+
+        // обновить файл нормативного документа на сервере
+        [HttpGet]
+        public async Task<IActionResult> UpdateIndustrySolution(string id)
+        {
+            var IS = await _db.ReadIndustrySolution(id);
+            return View(IS);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateIndustrySolution(string id, KB_industrySolution nIS, IFormFile newUploadedIS)
+        {
+            if (ModelState.IsValid & newUploadedIS != null)
+            {
+                var currentIS = await _db.ReadIndustrySolution(id);
+
+                // Определение пути по текущему файлу нормативного документа
+                string pathToDelete = currentIS.is_Path;
+                string pathToUpdate = FilesPath + newUploadedIS.FileName;
+
+                // удаление файла нормативного документа с сервера
+                FileInfo fi = new FileInfo(_appEnvironment.WebRootPath + pathToDelete);
+                if (fi.Exists)
+                {
+                    fi.Delete();
+                }
+
+                // Сохранение файла на сервере
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + pathToUpdate, FileMode.Create))
+                {
+                    await newUploadedIS.CopyToAsync(fileStream);
+                }
+
+                // Сохранение в БД
+                // Надпись
+                if (nIS.is_Name == null || nIS.is_Name.Length == 0)
+                {
+                    nIS.is_Name = newUploadedIS.FileName;
+                }
+
+                // иконка
+                var ext = newUploadedIS.FileName.Split('.').Last();
+                string icon_path = "/resources/partial/text.png";
+                if (ext.Equals("pdf"))
+                    icon_path = "/resources/partial/pdf.png";
+                else if (ext.Equals("doc"))
+                    icon_path = "/resources/partial/word.png";
+                else if (ext.Equals("zip"))
+                    icon_path = "/resources/partial/zip.png";
+                else if (ext.Equals("xls"))
+                    icon_path = "/resources/partial/excel.png";
+
+                nIS.is_IconPath = icon_path;
+
+                // путь к файлу
+                nIS.is_Path = pathToUpdate;
+
+                // id
+                nIS.Id = id;
+
+                await _db.UpdateIndustrySolution(nIS);
+                return RedirectToAction("Index", "Panel");
+            }
+            return View(nIS);
+        }
+
+
+        // удалить файл сертификата с сервера
+        [HttpGet]
+        public async Task<IActionResult> DeleteIndustrySolution(string id)
+        {
+            var currentIS = await _db.ReadIndustrySolution(id);
+
+            // Определение пути
+            string pathToDelete = currentIS.is_Path;
+
+            // удаление файла с сервера
+            FileInfo fi = new FileInfo(_appEnvironment.WebRootPath + pathToDelete);
+            if (fi.Exists)
+            {
+                fi.Delete();
+            }
+
+            // удаление файла из Бд
+            await _db.DeleteIndustrySolution(id);
+
+            return RedirectToAction("Index", "Panel");
+        }
+
+        #endregion
     }
 }
