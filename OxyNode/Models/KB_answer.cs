e522666e70ab48d91621262f007fa4c924144031
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-05-05 16:23:23 +0300

    откорректировал модели QA в соответствии с MongoDb

--- OxyNode/Models/KB_answer.cs
+++ OxyNode/Models/KB_answer.cs
@@ -2,12 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+
 // касается вопрос-ответ
 // модель ответа
-
 namespace OxyNode.Models
 {
     public class KB_answer
     {
+        // Id для хранения в MongoDb
+        [BsonRepresentation(BsonType.ObjectId)]
+        [HiddenInput(DisplayValue = false)]
+        string Id { get; set; }
+
+        // ФИО вопросителя - текст
+        [Display(Name = "ФИО")]
+        [StringLength(60, MinimumLength = 4, ErrorMessage = "ABJ иметь от 4 до 60 символов")]
+        string FullNameQestionary { get; set; }
+
+        // содержание - текст
+        [Display(Name = "Содержание вопроса")]
+        [Required(ErrorMessage = "Содержание обязательно", AllowEmptyStrings = false)]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Название должно иметь от 10 до 200 символов")]
+        string questionText { get; set; }
+
+        // содержание - текст
+        [Display(Name = "Содержание вопроса")]
+        [Required(ErrorMessage = "Содержание обязательно", AllowEmptyStrings = false)]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Название должно иметь от 10 до 200 символов")]
+        string answerText { get; set; }
+
+        // флаг публикации - если True - ответ виден на сайте
+        [Display(Name = "Флаг публикации на сайт")]
+        bool publishToSite { get; set; }
+
     }
 }
--- OxyNode/Models/KB_question.cs
+++ OxyNode/Models/KB_question.cs
@@ -2,12 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+
 // касается вопрос-ответ
 // модель вопроса
-
 namespace OxyNode.Models
 {
     public class KB_question
     {
+        // Id для хранения в MongoDb
+        [BsonRepresentation(BsonType.ObjectId)]
+        [HiddenInput(DisplayValue = false)]
+        string Id { get; set; }
+
+        // ФИО вопросителя - текст
+        [Display(Name = "ФИО")]
+        [StringLength(60, MinimumLength = 4, ErrorMessage = "ABJ иметь от 4 до 60 символов")]
+        string FullName { get; set; }
+
+        // содержание - текст
+        [Display(Name = "Содержание вопроса")]
+        [Required(ErrorMessage = "Содержание обязательно", AllowEmptyStrings = false)]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Название должно иметь от 10 до 200 символов")]
+        string questionText { get; set; }
+
+        // e-mail
+        [Display(Name = "e-mail")]
+        [StringLength(60, MinimumLength = 6, ErrorMessage = "Название должно иметь от 6 до 60 символов")]
+        string e_mail { get; set; }
+
     }
 }
