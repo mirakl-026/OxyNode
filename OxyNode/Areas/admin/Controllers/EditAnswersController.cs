efbda3bb8f6e28d1c949eafd6757e3f80f4cbd92
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-05-16 19:37:02 +0300

    Добавил ответы и взаимные ссылки в представлениях

--- OxyNode/Areas/admin/Controllers/EditAnswersController.cs
+++ OxyNode/Areas/admin/Controllers/EditAnswersController.cs
@@ -4,14 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using OxyNode.Services;
+using OxyNode.Models;
+using OxyNode.ViewModels;
+
 namespace OxyNode.Areas.admin.Controllers
 {
     [Area("admin")]
     public class EditAnswersController : Controller
     {
+        private KB_answerService _dbA;
+        private KB_questionService _dbQ;
+
+        public EditAnswersController(KB_answerService contextA, KB_questionService contextQ)
+        {
+            _dbA = contextA;
+            _dbQ = contextQ;
+        }
+
         // вывести список всех ответов
 
         // посмотреть конкретный ответ
+        [HttpGet]
+        public async Task<IActionResult> ReadOneAnswer(string answerId)
+        {
+            var a = await _dbA.ReadAnswer(answerId);
+
+            var q = await _dbQ.ReadQuestion(a.QuestionId);
+            ViewData["questionId"] = q.Id;
+            ViewData["questionFullName"] = q.FullName;
+            ViewData["questionAddress"] = q.Address;
+            ViewData["questionEmail"] = q.e_mail;
+            ViewData["questionText"] = q.questionText;
+
+            return View(a);
+        }
 
         // редактировать ответ
 
