1ea58f5f2f3437874d1beba0cc23664192b4a694
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-05-24 16:48:37 +0300

    Ветка новостей - добавил  инфраструктуру - файлы  контроллера, модела, вьюмодели, сервиса и базового представления

---
+++ OxyNode/Controllers/NewsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace OxyNode.Controllers
+{
+    // Контроллер новостей
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
