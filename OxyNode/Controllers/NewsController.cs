3f6c5fa089b00a890970c70cd5c32890a13acbd4
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-05-24 17:37:18 +0300

    Реализовал контроллер для главной страницы относительно новостей

--- OxyNode/Controllers/NewsController.cs
+++ OxyNode/Controllers/NewsController.cs
@@ -4,15 +4,68 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using OxyNode.Services;
+using OxyNode.Models;
+using OxyNode.ViewModels;
+
 
 namespace OxyNode.Controllers
 {
     // Контроллер новостей
     public class NewsController : Controller
     {
-        public IActionResult Index()
+        private NewsService _db;
+        public int pageSize = 6;
+
+        public NewsController (NewsService context)
         {
-            return View();
+            _db = context;
         }
+
+        // главная страница новостей
+        public async Task<IActionResult> Index()
+        {
+            // Вьюмодель для статей
+            NewsItemsViewModel nivm = new NewsItemsViewModel();
+
+            // передача во вьюмодель - общего ко-ва новостей
+            nivm.newsItemsCount = await _db.GetNewsItemsCount();
+
+            // номер текущей страницы
+            nivm.currentPageNumber = 1;
+
+            // сами новости
+            nivm.newsItems = await _db.GetPageOfNewsItems(1, pageSize);
+
+            return View(nivm);
+        }
+
+        // страница новостей pageSize
+        public async Task<IActionResult> Page(int pageNumber)
+        {
+            // Вьюмодель для статей
+            NewsItemsViewModel nivm = new NewsItemsViewModel();
+
+            // передача во вьюмодель - общего ко-ва новостей
+            nivm.newsItemsCount = await _db.GetNewsItemsCount();
+
+            // номер текущей страницы
+            nivm.currentPageNumber = 1;
+
+            // сами новости
+            nivm.newsItems = await _db.GetPageOfNewsItems(pageNumber, pageSize);
+
+            return View(nivm);
+        }
+
+
+        // страница одной новости
+        public async Task<IActionResult> ReadNews(string id)
+        {
+            NewsItem ni = await _db.ReadNewsItem(id);
+            return View(ni);
+        }
+
+
     }
 }
