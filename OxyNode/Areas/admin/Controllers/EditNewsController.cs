2557915dc0bfd948ca927fe01d6a74e0a694353f
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-05-24 17:18:20 +0300

    Добавил контроллер управления новостями в админку

--- OxyNode/Areas/admin/Controllers/EditNewsController.cs
+++ OxyNode/Areas/admin/Controllers/EditNewsController.cs
@@ -4,13 +4,95 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using OxyNode.Services;
+using OxyNode.ViewModels;
+using OxyNode.Models;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using MongoDB.Bson;
+
+
 namespace OxyNode.Areas.admin.Controllers
 {
+    [Area("admin")]
     public class EditNewsController : Controller
     {
-        public IActionResult Index()
+        private NewsService _db;
+
+        public EditNewsController(NewsService context)
+        {
+            _db = context;
+        }
+
+        // получить все новости
+        public async Task<IActionResult> ReadAllNews()
+        {
+            // вьюмодель
+            NewsItemsViewModel nivm = new NewsItemsViewModel();
+            nivm.newsItems = await _db.GetAllNewsItems();
+
+            nivm.currentPageNumber = 1;
+
+            nivm.newsItemsCount = await _db.GetNewsItemsCount();
+
+            return View(nivm);
+        }
+
+
+        #region CRUD
+        // Create - создать новость
+        [HttpGet]
+        public IActionResult CreateNews()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNews(NewsItem ni)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.CreateNewsItem(ni);
+                return RedirectToAction("ReadAllNews", "EditNews");
+            }
+            return View(ni);
+        }
+
+        // Read - страница новости
+        [HttpGet]
+        public async Task<IActionResult> ReadNews(string id)
+        {
+            var ni = await _db.ReadNewsItem(id);
+            return View(ni);
+        }
+
+        // Update - изменить новость
+        [HttpGet]
+        public async Task<IActionResult> UpdateNews(string id)
+        {
+            var niToUpdate = await _db.ReadNewsItem(id);
+            return View(niToUpdate);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateNews(NewsItem updatedNi)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.UpdateNewsItem(updatedNi);
+                return RedirectToAction("ReadAllNews", "EditNews");
+            }
+            return View(updatedNi);
+        }
+
+        // Delete
+        [HttpGet]
+        public async Task<IActionResult> DeleteNews(string id)
+        {
+            await _db.DeleteNewsItem(id);
+            return RedirectToAction("ReadAllNews", "EditNews");
+        }
+        #endregion
     }
 }
