70fd4f87696e9f88b4500d77d48478c37eb9dd41
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-05-31 17:58:50 +0300

    Добавил файлы контроллеры - Услуги, Услуги/Ремонт, Услуги/Калибровка, Услуги/Поверка

---
+++ OxyNode/Controllers/VerificationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// контроллер -> услуги | поверка
+namespace OxyNode.Controllers
+{
+    public class VerificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
