4a903b0726b7107b65f58b982bf78b339760e397
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-05-31 12:24:32 +0300

    Добавил файлы для реализаций интерфейсов

---
+++ OxyNode/Services/MongoDB/MDB_KB_noteService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OxyNode.Services.MongoDB
+{
+    public class MDB_KB_noteService
+    {
+    }
+}
