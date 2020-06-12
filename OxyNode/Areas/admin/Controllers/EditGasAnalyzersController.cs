cfc850f876d907c726076f0be3256b05d99f4abf
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-06-12 16:40:17 +0300

    Добавил заглушки для админки ГА

---
+++ OxyNode/Areas/admin/Controllers/EditGasAnalyzersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// все действия над газоанализаторами
+
+namespace OxyNode.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class EditGasAnalyzersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
