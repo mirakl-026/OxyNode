2a15f8ded17699a5236621d55309ed264ed2e034
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-06-24 14:55:41 +0300

    объединяю функционал вопрос - ответ в единую модель, интерфейс и сервис

---
+++ OxyNode/Models/KB_QA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// объединённая модель - вопрос + ответ
+
+namespace OxyNode.Models
+{
+    public class KB_QA
+    {
+    }
+}
