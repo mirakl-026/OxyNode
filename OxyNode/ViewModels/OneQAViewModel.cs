d57bd7324e8cd02a48f26d7abf295bfe5dbe04b7
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-06-24 15:23:43 +0300

    Изменил Вью-модели, удалил представления главной

--- OxyNode/ViewModels/OneQAViewModel.cs
+++ OxyNode/ViewModels/OneQAViewModel.cs
@@ -5,14 +5,10 @@ using System.Threading.Tasks;
 
 using OxyNode.Models;
 
-
-// вьюмодель одной пары - вопрос+ответ
 namespace OxyNode.ViewModels
 {
     public class OneQAViewModel
     {
-        public KB_question question;
-
-        public KB_answer answer;
+        public KB_QA qa { get; set; }
     }
 }
