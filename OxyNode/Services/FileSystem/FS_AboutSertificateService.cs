33e9bc76cddf81c1096f7987fcaa0f33438498b7
mirakl-026 <57499685+mirakl-026@users.noreply.github.com>
2020-07-17 15:14:33 +0300

    добавил файлы для реализации интерфейсов ФС (кроме крневого)

---
+++ OxyNode/Services/FileSystem/FS_AboutSertificateService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using OxyNode.Infrastructure.Interfaces.FileSystem;
+
+namespace OxyNode.Services.FileSystem
+{
+    public class FS_AboutSertificateService : IFileAboutSertificateService
+    {
+        public Task AddAboutSertificate(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAboutSertificate(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAllAboutSertificates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAboutSertificatesFilesList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OxyNode/Services/FileSystem/FS_ImageService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using OxyNode.Infrastructure.Interfaces.FileSystem;
+
+namespace OxyNode.Services.FileSystem
+{
+    public class FS_ImageService : IFileImageService
+    {
+        public Task AddImage(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAllImages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteImage(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetImagesFilesList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
